c287db164ce5d114a8f620e561a2886f819b7a21
Hoc Le <viethocpro@gmail.com>
2023-03-19 23:19:26 +0700

    Use psql instead sqlight

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_18_040339) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "urls", force: :cascade do |t|
     t.string "short_url"
     t.string "original_url"
